b4dddf4aaa20aff0ce1a7e2995eb8b52feb01b98
avillegasa <andres.villegasa19@gmail.com>
2023-11-24 01:53:14 -0400

    Eliminando imports, estructuras inservibles.

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -3,11 +3,9 @@ import React, { useState, useEffect } from 'react';
 import ProductList from './ProductList';
 import Navbar from './Navbar';
 import Menu from './Menu';
-import Pasteles from './Pasteles';
-import './Principal.css';
+import Destacados from './Destacados';
+import './styles/Principal.css';
 import BotonCafe from './BotonCafe';
-import BotonSecion from './Iniciarsecion';
-import { fetchProducts } from '../assets/api';
 import SinCuenta from './SinCuenta';
 import EntradaTexto from './EntradaTexto';
 import ConCuenta from './ConCuenta';
@@ -15,43 +13,15 @@ import ConCuenta from './ConCuenta';
 
 
 const Principal = () => {
-  const [products, setProducts] = useState([]);
-  const [showPasteles, setShowPasteles] = useState(false);
-
-  useEffect(() => {
-    fetchProducts().then((data) => setProducts(data));
-  }, []);
-
-  const handleMenuClick = (menuItem) => {
-    if (menuItem === 'pasteles') {
-      setShowPasteles(true);
-    } else {
-      setShowPasteles(false);
-    }
-  };
-
+  
   return (
     <div className="principal">
       <Navbar />
-      
-      <Menu onMenuClick={handleMenuClick} />
-      <main>
-        {!showPasteles ? (
-          <ProductList products={products} />
-        ) : (
-          <Pasteles />
-        )}
-      </main>
-      
+      <Menu />
       <BotonCafe>Inicia Sesión</BotonCafe>
-      <BotonSecion/>
       <SinCuenta/>
       <EntradaTexto Texto="dil" />
-      <ConCuenta/>
-      
-      
-     
-      
+      <ConCuenta/> 
     </div>
   );
 };
