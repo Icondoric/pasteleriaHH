d11c66a5fe1d1011502b8e226a6366b16a0adb3d
avillegasa <andres.villegasa19@gmail.com>
2023-11-24 01:51:24 -0400

    Cambiando la estruc. de Destacados, antes pasteles

---
+++ src/components/Destacados.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+    const Destacados = () => {
+  return (
+    <div>
+      <h2>¡Aqui deberia entrar la seccion de Destacados!</h2>
+    </div>
+  );
+};
+
+export default Destacados;
