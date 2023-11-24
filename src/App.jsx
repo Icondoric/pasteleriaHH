3cb78d77354ddea07df78587a36cbba3b1066703
avillegasa <andres.villegasa19@gmail.com>
2023-11-24 05:37:31 -0400

    Editando imports e integrando rutas de navegacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 // App.js
 import React from 'react'
 import {Routes, Route, Outlet, Link} from 'react-router-dom'
-import PanelNavegacion from './components/PanelNavegacion'
 import './App.css'
 import Login from './components/template/Login'
 import Menu from './components/Menu'
 import Principal from './components/Principal'
-
+import CategoriaPasteles from './components/CategoriaPasteles'
+import CategoriaMuffins from './components/CategoriaMuffins'
+import CategoriaDonas from './components/CategoriaDonas'
+import {infoMuffins} from './assets/muffinsApi'
+import {pastel} from './assets/pastelesAPI'
+import {infodonas} from './assets/donasAPI'
 
 
 const App = () => {
@@ -15,10 +19,11 @@ const App = () => {
     <Routes>
         <Route path='/' element={<Login />}/>
         <Route path='/principal' element = {<Principal />}/>
-        <Route path='menu-pasteles' element = {<Menu />} />
-        <Route path='menu-muffins' element = {<Menu />} />
-        <Route path='menu-donas' element = {<Menu />} />
-        <Route path='menu-panes' element = {<Menu />} />
+        <Route path='/menu-pasteles' element = {<CategoriaPasteles pastel={pastel}/>} />
+        <Route path='/menu-muffins' element = {<CategoriaMuffins infoMuffins = {infoMuffins}/>} />
+        <Route path='/menu-donas' element = {<CategoriaDonas infodonas = {infodonas}/>} />
+        <Route path='/menu-panes' element = {<Menu />} /> 
+        <Route path='/atras' element = {<Principal />}/>
     </Routes> 
     </div>
   );
