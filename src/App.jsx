aedd8cbc842819ad158c84db3e0ed5a2dbb2fc6a
avillegasa <andres.villegasa19@gmail.com>
2023-11-24 01:46:39 -0400

    Cambiando los imports para el uso de reactroutedom

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 // App.js
-import React from 'react';
-
-import PanelNavegacion from './components/PanelNavegacion';
-import './App.css';
-import Login from './components/template/Login';
-import Registrarse from './components/template/Registrarse';
-import DireccDelibery from './components/template/DireccDelibery';
+import React from 'react'
+import {Routes, Route, Outlet, Link} from 'react-router-dom'
+import PanelNavegacion from './components/PanelNavegacion'
+import './App.css'
+import Login from './components/template/Login'
+import Registrarse from './components/template/Registrarse'
+import DireccDelibery from './components/template/DireccDelibery'
+import Principal from './components/Principal'
 
 
 const App = () => {
   return (
     <div className="app-container">
-      
-      <Registrarse/>
+    <Routes>
+        <Route path='/' element={<Login />}/>
+        <Route path='/principal' element = {<Principal />}/>
+    </Routes> 
     </div>
   );
 };
