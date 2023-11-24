c6901258d0b65897be1855dfd0d747fd184f6146
avillegasa <andres.villegasa19@gmail.com>
2023-11-24 02:17:15 -0400

    Integrando Import y Links

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,22 +1,31 @@
 // Menu.js
-import React from "react";
-import "./styles/Menu.css"; // Importa los estilos
+import React from "react"
+import "./styles/Menu.css"
+import {Link} from 'react-router-dom'
 
 const Menu = () => {
   return (
     <div className="menu">
-      <div onClick={() => handleMenuClick("pasteles")}>
+    <Link to='/menu-pasteles'>
+      <div onClick={() => (window.location.href = "pasteles")}>
         <img src="/pasteles.svg" alt="Pasteles" />
       </div>
+    </Link>
+    <Link to = '/menu-muffins'>
       <div onClick={() => (window.location.href = "/chescomufins")}>
         <img src="/chesco.svg" alt="ChescoMufins" />
       </div>
+    </Link>
+    <Link to = '/menu-donas'>
       <div onClick={() => (window.location.href = "/donas")}>
         <img src="/donas.svg" alt="Donas" />
       </div>
+    </Link>
+    <Link to = 'menu-panes'>
       <div onClick={() => (window.location.href = "/panes")}>
         <img src="/marrana.svg" alt="Panes" />
       </div>
+    </Link>
     </div>
   );
 };
