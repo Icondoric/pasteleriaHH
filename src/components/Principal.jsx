210c8f7d7914e661826544dd66a598eb386c12a8
avillegasa <andres.villegasa19@gmail.com>
2023-11-24 05:45:45 -0400

    Integrando Seccion Destacados y su API a Principal

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -2,7 +2,9 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Menu from './Menu';
+import Destacados from './Destacados';
 import './styles/Principal.css';
+import {postredestacado} from '../assets/DestacadosAPI';
 
 const Principal = () => {
   
@@ -10,7 +12,7 @@ const Principal = () => {
     <div className="principal">
       <Navbar />
       <Menu />
-      
+      <Destacados postredestacado = {postredestacado}/>
     </div>
   );
 };
