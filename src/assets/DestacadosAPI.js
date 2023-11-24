657ad439d2925db961d2cb90305cedff5a0e6f1f
avillegasa <andres.villegasa19@gmail.com>
2023-11-24 05:38:50 -0400

    Creando la API destacados

---
+++ src/assets/DestacadosAPI.js
@@ -0,0 +1,30 @@
+export const postredestacado = [
+    {
+        titulo: 'Dona de mango',
+        descripcion: 'postre esponjoso y dulce hecho con harina, azúcar, huevos, mantequilla y esencia de vainilla...',
+        precio: '15 Bs. ',
+        imgUrl: '/dona de mango.svg',
+        disponible: 'Disponible x1'
+    },
+    {
+        titulo: "Pastel Red Velvet",
+        descripcion:"El pastel Red Velvet es conocido por su suave textura y su característico color ro...",
+        imgUrl: "/Red_Velvet.png",
+        precio:"125 Bs.",
+        disponible:"Disponible x1",
+    },
+    {
+        titulo: 'Dona de limón',
+        descripcion: 'postre esponjoso y dulce hecho con harina, azúcar, huevos, mantequilla y esencia de vainilla...',
+        precio: '15 Bs. ',
+        imgUrl: '/312dona 1 (2).svg',
+        disponible: 'Disponible x11'
+    },
+    {
+        titulo: 'Dona de limón',
+        descripcion: 'Una dona esponjosa y ligera con un glaseado de limón que aporta la cantidad justa de dul...',
+        precio: '10 Bs. ',
+        imgUrl: '/312dona 1 (2).svg',
+        disponible: 'Disponible x15'
+    },
+  ]
