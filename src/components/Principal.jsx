d58019c8efefcd0a3f1d71ac8b2f9b4777c8573e
avillegasa <andres.villegasa19@gmail.com>
2023-11-24 01:58:57 -0400

    Integrando solo lo correspondiente segun figma

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -1,16 +1,8 @@
 // Principal.js
-import React, { useState, useEffect } from 'react';
-import ProductList from './ProductList';
+import React from 'react';
 import Navbar from './Navbar';
 import Menu from './Menu';
-import Destacados from './Destacados';
 import './styles/Principal.css';
-import BotonCafe from './BotonCafe';
-import SinCuenta from './SinCuenta';
-import EntradaTexto from './EntradaTexto';
-import ConCuenta from './ConCuenta';
-
-
 
 const Principal = () => {
   
@@ -18,10 +10,7 @@ const Principal = () => {
     <div className="principal">
       <Navbar />
       <Menu />
-      <BotonCafe>Inicia Sesión</BotonCafe>
-      <SinCuenta/>
-      <EntradaTexto Texto="dil" />
-      <ConCuenta/> 
+      
     </div>
   );
 };
